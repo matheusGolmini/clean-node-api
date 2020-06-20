b0757ec7a4ba054225bbd4b1f1d42b819f2ce1f0
Matheus Fernando Golmini <matheusgolminif@gmail.com>
2020-06-20 16:04:07 -0300

    feat: make SignUpCOntroller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,16 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
+export class SignUpController implements Controller {
     handle (httpRequest: HttpRequest): HttpResponse {
         const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
         for (const field of requiredFields) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
             }
         }
-
         return {
             statusCode: 200,
             body: ''
